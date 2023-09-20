5d2f6413c0d55f1158f56fa48ff1b3146b47ad42
Oleksy Lykov <lukov_jr@ukr.net>
2023-09-20 15:47:59 +0300

    tetetet

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
         borderRadius: 4,
         backgroundColor: "green",
         color: "red",
-      }}>deployDDDDDDDDDDD</h1> 
+      }}>deployDDD</h1> 
   // </React.StrictMode>
 );
 
